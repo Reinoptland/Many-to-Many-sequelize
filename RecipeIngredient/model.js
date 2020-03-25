015205562a9048ef906fba9dbca78354aefbf6f3
Reinoptland <reinoptland@gmail.com>
2020-03-25 14:10:25 +0100

    Define relationships

--- RecipeIngredient/model.js
+++ RecipeIngredient/model.js
@@ -1,10 +1,17 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
+const Ingredient = require("../Ingredient/model");
+const Recipe = require("../Recipe/model");
 
 const RecipeIngredient = db.define("recipe_ingredient", {
-  recipeId: Sequelize.INTEGER,
-  ingredientId: Sequelize.INTEGER,
   optionalIngredient: Sequelize.BOOLEAN
 });
+
+Ingredient.hasMany(RecipeIngredient);
+Recipe.hasMany(RecipeIngredient);
+RecipeIngredient.belongsTo(Recipe);
+RecipeIngredient.belongsTo(Ingredient);
+// Do i need this one as well?
+// Ingredient.belongsToMany(Recipe)
 
 module.exports = RecipeIngredient;
